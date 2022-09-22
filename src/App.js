a211258903255d2a4587e66b675ecf5c18329d00
Farrel Abyansyah <farrel.abyansyah@gmail.com>
2022-09-22 18:56:21 +0700

    add routing functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
-import SolarContent from "./components/Solar_panel/SolarContent";
 import Navbar from "./components/Navbar/Navbar";
+import SolarContent from "./components/Solar_panel/SolarContent";
+import WindContent from "./components/Wind_turbine/WindContent";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <SolarContent />
+    <Navbar />
+    <Routes>
+      <Route path= "/pv" element= {<SolarContent />} />
+      <Route path= "/wind" element= {<WindContent />}/>
+    </Routes>
     </>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Navbar.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFan, faSolarPanel, faChartLine } from "@fortawesome/free-solid-svg-icons";
-
-
-const logo_width = 30;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFan,
+  faSolarPanel,
+  faChartLine,
+} from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -12,22 +14,28 @@ function Navbar() {
       <div className="nav-container">
         <div className="nav-item">
           <div className="nav-img">
-            <FontAwesomeIcon icon={faChartLine} size='xl' />
+            <FontAwesomeIcon icon={faChartLine} size="xl" />
           </div>
           <div className="nav-text">Dashboard</div>
         </div>
-        <div className="nav-item">
-          <div className="nav-img">
-            <FontAwesomeIcon icon={faSolarPanel} size='xl' />
+
+        <NavLink to="/pv">
+          <div className="nav-item">
+            <div className="nav-img">
+              <FontAwesomeIcon icon={faSolarPanel} size="xl" />
+            </div>
+            <div className="nav-text">Solar Panel</div>
           </div>
-          <div className="nav-text">Solar Panel</div>
-        </div>
-        <div className="nav-item">
-          <div className="nav-img">
-            <FontAwesomeIcon icon={faFan} size='xl'/>
+        </NavLink>
+
+        <NavLink to="/wind">
+          <div className="nav-item">
+            <div className="nav-img">
+              <FontAwesomeIcon icon={faFan} size="xl" />
+            </div>
+            <div className="nav-text">Wind Turbine</div>
           </div>
-          <div className="nav-text">Wind Turbine</div>
-        </div>
+        </NavLink>
       </div>
     </nav>
   );
